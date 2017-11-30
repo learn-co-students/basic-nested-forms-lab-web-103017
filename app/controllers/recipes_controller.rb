54f54f050e44748848fb705cb2a4a68560a5120f
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-11-29 21:17:19 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'one')
+    @recipe.ingredients.build(name: 'two')
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+     redirect_to recipes_path
+  end
+
+  def recipe_params
+       params.require(:recipe).permit(
+         :title,
+         ingredients_attributes: [
+           :name,
+           :quantity
+         ] )
   end
 end
